99c56a827749bc50818560a8ba627c6e4b8b00b0
gambler <gambler>
2022-12-22 19:56:23 -0600

    input validation

--- main.go
+++ main.go
@@ -5,15 +5,8 @@ import (
 	"os"
 )
 
-func checkArgs(args []string) {
-	if len(args) <= 1 {
-		fmt.Println("invalid numargs")
-		os.Exit(1)
-	}
-}
-
 func main() {
-	checkArgs(os.Args)
+	checkArgs()
 
 	switch os.Args[1] {
 	case "--create-wallet":
@@ -31,5 +24,41 @@ func main() {
 	case "--send-eth":
 		txid := sendEth(os.Args[2], os.Args[3], os.Args[4])
 		fmt.Println("transaction id: " + txid)
+	default:
+		printUsage()
 	}
+}
+
+func checkArgs() {
+	if len(os.Args) <= 1 {
+		printUsage()
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "--create-wallet":
+		if len(os.Args) != 2 {
+			fmt.Println("option '--create-wallet' does not take any extra arguments")
+			printUsage()
+			os.Exit(1)
+		}
+	case "--check-balance":
+		if len(os.Args) != 3 {
+			fmt.Println("option '--check-balance' requires an ethereum address as an argument")
+			printUsage()
+			os.Exit(1)
+		}
+	case "--send-eth":
+		if len(os.Args) != 5 {
+			fmt.Println("option '--send-eth' requires a private key, amount, and to address as arguments")
+			printUsage()
+			os.Exit(1)
+		}
+	default:
+		printUsage()
+	}
+}
+
+func printUsage() {
+	fmt.Println("usage: eth-tool [--create-wallet] [--check-balance <address>] [--send-ether <privkey> <amount> <toAddress>]")
 }
